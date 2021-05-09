822ead927ea53ed1da0453fa3ca9e65ebe81db4c
Henrik <henrik@teinelund.se>
2021-05-09 16:16:30 +0200

    Implemented verify parameters.

--- src/main/java/org/teinelund/tools/c2plantuml/Application.java
+++ src/main/java/org/teinelund/tools/c2plantuml/Application.java
@@ -3,10 +3,20 @@ package org.teinelund.tools.c2plantuml;
 import com.beust.jcommander.JCommander;
 import com.beust.jcommander.Parameter;
 
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Objects;
+
 /**
  * Main class
  */
 public class Application {
+
+    @Parameter(names = { "-i", "--input" }, description = "Directory containing c source code to be analyzed. Mandatory.", order = 1)
+    private String input = "";
+
+    @Parameter(names = { "-o", "--output" }, description = "Output file path to store plant UML content. Mandatory.", order = 2)
+    private String output = "";
 
     @Parameter(names = { "-v", "--verbose" }, description = "Verbose output.", order = 50)
     private boolean verbose = false;
@@ -50,7 +60,34 @@ public class Application {
 
         printVerbose("Verbose mode on.");
 
-        System.out.println("Welcome to c2plantuml!");
+        verifyParameters();
+    }
+
+    void verifyParameters() {
+        if (Objects.isNull(input) || input.isBlank()) {
+            printError("Parameter --input is mandatory.");
+            System.exit(1);
+        }
+        if (Objects.isNull(output) || output.isBlank()) {
+            printError("Parameter --output is mandatory.");
+            System.exit(1);
+        }
+
+        Path inputPath = Path.of(input);
+        if (Files.notExists(inputPath)) {
+            printError("Input path '" + input + "' does not exist. Check spelling.");
+            System.exit(1);
+        }
+        if (!Files.isDirectory(inputPath)) {
+            printError("Input path '" + input + "' is not a directory. Check it.");
+            System.exit(1);
+        }
+
+        Path outputPath = Path.of(output);
+        if (Files.exists(outputPath)) {
+            printError("Output path '" + output + "' does exist. Check it.");
+            System.exit(1);
+        }
     }
 
     static void printInfo(String message) {
